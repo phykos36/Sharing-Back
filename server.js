924e4a7368cc7d135ce0a90c3b5f30a353b6e99b
Phykos36 <furutaku12@gmail.com>
2019-10-10 12:13:03 +0900

    using express

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1'; //unused
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/',(req, res) => {
+  res.writeHead(200);
+  res.write('Hello World');
+  res.end();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
