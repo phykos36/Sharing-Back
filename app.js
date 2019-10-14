cd7652ee9f4c2cba1d23c7287bbce3638565d032
Phykos36 <furutaku12@gmail.com>
2019-10-15 00:25:52 +0900

    change json to query

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
-
+const hostname = '127.0.0.1'; 
+const port = 8124;
+const connection = mysql.createConnection({
+    host    :  hostname,
+    user    : 'master',
+    password: 'nodemaria',
+    database: 'studydb'
+  });
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 })
 app.post('/',function(req, res){
-    var obj = {}; 
-    console.log('body: ' + JSON.stringify(req.body.name));
-    var rejson = JSON.stringify(req.body);
-    res.send(rejson);
+  var obj = {}; 
+  var queryname = "\"" + JSON.stringify(req.body.username) + "\"";
+  console.log('username: ' + queryname);
+  var rejson;
+  connection.query("SELECT * FROM Person WHERE name = \"Hanako\"",[queryname],
+  function(error, results, fields){
+    console.log(results);
+    rejson = results;
+  });
+  res.send(rejson);
 });
 
-app.listen(8124, () => console.log('listening on port 8124'));
+app.listen(port, () => console.log('listening on port %d', port));
