04257cb59b21bad02a6143f9d3ca00ebbed55664
Phykos36 <furutaku12@gmail.com>
2019-10-14 23:40:18 +0900

    rending public/index.html

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const http = require('http');
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
-const hostname = '127.0.0.1'; //unused
+app.use(express.static('public'));
+const hostname = '127.0.0.1'; 
 const port = 3000;
 
-const connection = mysql.createConnection({
-  host    :  hostname,
-  user    : 'master',
-  password: 'nodemaria',
-  database: 'studydb'
-});
-//app.set('view engine');
-//app.use(express.static('views'));
 const recordLog = function(req, res, next){
   console.log('%o : %o にアクセスしました',hostname, port);
   next();
@@ -20,17 +12,6 @@ const recordLog = function(req, res, next){
 app.use(recordLog);
 
 app.get('/', function(req, res){
-  let query = 'select * from studydb.Person';
-  connection.query(query, (err, rows, fields) => {
-    if(err) throw err;
-    res.render('index', {name: 'node + mysql practice', age: rows});
-  });
+  res.sendfile('index.html');
 });
-/*
-app.get('/',(req, res) => {
-  res.writeHead(200);
-  res.write('Hello World');
-  res.end();
-});
-*/
-app.listen(port, () => console.log('listening on port 3000'));
+app.listen(port, () => console.log('listening on port %d', port));
